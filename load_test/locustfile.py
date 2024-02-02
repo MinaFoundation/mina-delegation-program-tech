d045caedd617d21e739e50700f9b9a14bd5b4c0f
Piotr Stachyra <piotr.stachyra@gmail.com>
2024-02-02 13:27:46 +0100

    add minimina payloads

--- load_test/locustfile.py
+++ load_test/locustfile.py
@@ -3,18 +3,24 @@ import os
 import random
 from locust import HttpUser, TaskSet, task, between
 
+# Get the test name from an environment variable
+TEST_NAME = os.environ.get("TEST_NAME", "default")
+
+# Construct the payload directory path based on the test name
+PAYLOAD_DIR = f"./payload/{TEST_NAME}"
+
 TARGET_EP = "http://localhost:8080/v1/submit"
 
 
 class UserBehavior(TaskSet):
     @task
     def submit_payload(self):
-        files = os.listdir("./payload")
+        files = os.listdir(PAYLOAD_DIR)
         files = [file for file in files if file.endswith(".json")]
         selected_file = random.choice(files)
 
         # Load the payload from the randomly selected file
-        with open(f"./payload/{selected_file}", "r") as file:
+        with open(f"{PAYLOAD_DIR}/{selected_file}", "r") as file:
             payload = json.load(file)
 
         headers = {"Content-Type": "application/json"}
