a934821fa1abae2e3c288c786076e6cb7c3a287c
Piotr Stachyra <piotr.stachyra@gmail.com>
2024-05-14 16:07:24 +0200

    formatting

--- db_migration/db_diff.py
+++ db_migration/db_diff.py
@@ -19,6 +19,7 @@ def batched(iterable, n):
     if items:
         yield items
 
+
 class Insert:
 
     def __init__(self, connection, table, columns):
@@ -28,35 +29,56 @@ class Insert:
         self.results = []
 
     def fetch(self, condition, args, joins=()):
-        cols = ', '.join('{}.{}'.format(self.table, col) for col in self.columns)
-        j = ' '.join('JOIN {tbl} AS {as} ON {as}.{col} = {val}'.format(**join) for join in joins)
-        q = 'SELECT DISTINCT {} FROM {} {} WHERE {} ORDER BY {}.{} LIMIT ALL'
+        cols = ", ".join("{}.{}".format(self.table, col) for col in self.columns)
+        j = " ".join(
+            "JOIN {tbl} AS {as} ON {as}.{col} = {val}".format(**join) for join in joins
+        )
+        q = "SELECT DISTINCT {} FROM {} {} WHERE {} ORDER BY {}.{} LIMIT ALL"
         with self.connection.cursor() as cursor:
-            cursor.execute(q.format(cols, self.table, j, condition, self.table, self.columns[0]), args)
+            cursor.execute(
+                q.format(cols, self.table, j, condition, self.table, self.columns[0]),
+                args,
+            )
             self.results += cursor.fetchall()
 
     def print(self):
         for batch in batched(self.results, 1000):
-            cols = ', '.join(self.columns)
-            print('INSERT INTO {}'.format(self.table))
-            print('    ({})'.format(cols))
-            print('VALUES')
-            print('   ', ',\n    '.join('({})'.format(', '.join(Literal(item).as_string(self.connection) for item in row)) for row in batch))
-            print('ON CONFLICT(id)')
-            print('DO UPDATE SET')
-            print('   ', ',\n    '.join('{col} = EXCLUDED.{col}'.format(col=col) for col in self.columns))
-            print(';')
+            cols = ", ".join(self.columns)
+            print("INSERT INTO {}".format(self.table))
+            print("    ({})".format(cols))
+            print("VALUES")
+            print(
+                "   ",
+                ",\n    ".join(
+                    "({})".format(
+                        ", ".join(
+                            Literal(item).as_string(self.connection) for item in row
+                        )
+                    )
+                    for row in batch
+                ),
+            )
+            print("ON CONFLICT(id)")
+            print("DO UPDATE SET")
+            print(
+                "   ",
+                ",\n    ".join(
+                    "{col} = EXCLUDED.{col}".format(col=col) for col in self.columns
+                ),
+            )
+            print(";")
 
 
 def parse_args():
-    p = argparse.ArgumentParser(description='Diff two uptime service databases.')
-    p.add_argument('-H', '--host', help='Database hostname', default='localhost')
-    p.add_argument('-p', '--port', help='Database port', default=5432, type=int)
-    p.add_argument('-U', '--user', help='Database username', default='postgres')
-    p.add_argument('-w', '--password', help='Database password', required=True)
-    p.add_argument('-d', '--database', help='Database name', required=True)
-    p.add_argument('last_update', help='Last update time', type=datetime.fromisoformat)
+    p = argparse.ArgumentParser(description="Diff two uptime service databases.")
+    p.add_argument("-H", "--host", help="Database hostname", default="localhost")
+    p.add_argument("-p", "--port", help="Database port", default=5432, type=int)
+    p.add_argument("-U", "--user", help="Database username", default="postgres")
+    p.add_argument("-w", "--password", help="Database password", required=True)
+    p.add_argument("-d", "--database", help="Database name", required=True)
+    p.add_argument("last_update", help="Last update time", type=datetime.fromisoformat)
     return p.parse_args()
+
 
 def main(args):
     conn = psycopg2.connect(
@@ -64,114 +86,137 @@ def main(args):
         password=args.password,
         host=args.host,
         port=args.port,
-        database=args.database
+        database=args.database,
     )
 
-    print('BEGIN;\n')
+    print("BEGIN;\n")
     bot_logs = Insert(
         conn,
-        'bot_logs',
-        ('id',
-         'files_processed',
-         'file_timestamps',
-         'batch_start_epoch',
-         'batch_end_epoch',
-         'processing_time')
+        "bot_logs",
+        (
+            "id",
+            "files_processed",
+            "file_timestamps",
+            "batch_start_epoch",
+            "batch_end_epoch",
+            "processing_time",
+        ),
     )
     bot_logs.fetch(
-        'bot_logs.batch_end_epoch >= trunc(extract(epoch from (%s)))',
-        (args.last_update, )
+        "bot_logs.batch_end_epoch >= trunc(extract(epoch from (%s)))",
+        (args.last_update,),
     )
     bot_logs.print()
 
-    statehash = Insert(conn, 'statehash', ('id', 'value'))
+    statehash = Insert(conn, "statehash", ("id", "value"))
     statehash.fetch(
-        'bl.batch_end_epoch >= trunc(extract(epoch from (%s)))',
-        (args.last_update, ),
-        joins=({'tbl': 'bot_logs_statehash', 'as': 'bls', 'col': 'statehash_id', 'val': 'statehash.id'},
-               {'tbl': 'bot_logs', 'as': 'bl', 'col': 'id', 'val': 'bls.bot_log_id'})
+        "bl.batch_end_epoch >= trunc(extract(epoch from (%s)))",
+        (args.last_update,),
+        joins=(
+            {
+                "tbl": "bot_logs_statehash",
+                "as": "bls",
+                "col": "statehash_id",
+                "val": "statehash.id",
+            },
+            {"tbl": "bot_logs", "as": "bl", "col": "id", "val": "bls.bot_log_id"},
+        ),
     )
     statehash.fetch(
-        'bl.batch_end_epoch >= trunc(extract(epoch from (%s)))',
-        (args.last_update, ),
-        joins=({'tbl': 'bot_logs_statehash', 'as': 'bls', 'col': 'parent_statehash_id', 'val': 'statehash.id'},
-               {'tbl': 'bot_logs', 'as': 'bl', 'col': 'id', 'val': 'bls.bot_log_id'})
+        "bl.batch_end_epoch >= trunc(extract(epoch from (%s)))",
+        (args.last_update,),
+        joins=(
+            {
+                "tbl": "bot_logs_statehash",
+                "as": "bls",
+                "col": "parent_statehash_id",
+                "val": "statehash.id",
+            },
+            {"tbl": "bot_logs", "as": "bl", "col": "id", "val": "bls.bot_log_id"},
+        ),
     )
     statehash.print()
 
     bot_logs_statehash = Insert(
         conn,
-        'bot_logs_statehash',
-        ('id',
-         'bot_log_id',
-         'statehash_id',
-         'parent_statehash_id',
-         'weight')
+        "bot_logs_statehash",
+        ("id", "bot_log_id", "statehash_id", "parent_statehash_id", "weight"),
     )
     bot_logs_statehash.fetch(
-        'bl.batch_end_epoch >= trunc(extract(epoch from (%s)))',
-        (args.last_update, ),
-        joins=({'tbl': 'bot_logs', 'as': 'bl', 'col': 'id', 'val': 'bot_logs_statehash.bot_log_id'}, )
+        "bl.batch_end_epoch >= trunc(extract(epoch from (%s)))",
+        (args.last_update,),
+        joins=(
+            {
+                "tbl": "bot_logs",
+                "as": "bl",
+                "col": "id",
+                "val": "bot_logs_statehash.bot_log_id",
+            },
+        ),
     )
     bot_logs_statehash.print()
 
     nodes = Insert(
         conn,
-        'nodes',
-        ('id',
-         'block_producer_key',
-         'score',
-         'score_percent',
-         'updated_at',
-         'email_id',
-         'application_status')
+        "nodes",
+        (
+            "id",
+            "block_producer_key",
+            "score",
+            "score_percent",
+            "updated_at",
+            "email_id",
+            "application_status",
+        ),
     )
     nodes.fetch(
-        'bl.batch_end_epoch >= trunc(extract(epoch from (%s))) AND updated_at IS NOT NULL',
-        (args.last_update, ),
-        joins=({'tbl': 'points', 'as': 'p', 'col': 'node_id', 'val': 'nodes.id'},
-               {'tbl': 'bot_logs', 'as': 'bl', 'col': 'id', 'val': 'p.bot_log_id'})
+        "bl.batch_end_epoch >= trunc(extract(epoch from (%s))) AND updated_at IS NOT NULL",
+        (args.last_update,),
+        joins=(
+            {"tbl": "points", "as": "p", "col": "node_id", "val": "nodes.id"},
+            {"tbl": "bot_logs", "as": "bl", "col": "id", "val": "p.bot_log_id"},
+        ),
     )
     for row in nodes.results:
         if row[4] is None:
             # we don't want NULLs in this column, so we have to insert something
             # it doesn't really matter much anyways.
-            row[4] = 'now()'
+            row[4] = "now()"
     nodes.print()
 
     points = Insert(
         conn,
-        'points',
-        ('id',
-         'file_name',
-         'blockchain_epoch',
-         'blockchain_height',
-         'created_at',
-         'amount',
-         'node_id',
-         'bot_log_id',
-         'file_timestamps',
-         'statehash_id')
+        "points",
+        (
+            "id",
+            "file_name",
+            "blockchain_epoch",
+            "blockchain_height",
+            "created_at",
+            "amount",
+            "node_id",
+            "bot_log_id",
+            "file_timestamps",
+            "statehash_id",
+        ),
     )
     points.fetch(
-        'bl.batch_end_epoch >= trunc(extract(epoch from (%s)))',
-        (args.last_update, ),
-        joins=({'tbl': 'bot_logs', 'as': 'bl', 'col': 'id', 'val': 'points.bot_log_id'}, )
+        "bl.batch_end_epoch >= trunc(extract(epoch from (%s)))",
+        (args.last_update,),
+        joins=(
+            {"tbl": "bot_logs", "as": "bl", "col": "id", "val": "points.bot_log_id"},
+        ),
     )
     points.print()
 
     score_history = Insert(
-        conn,
-        'score_history',
-        ('node_id',
-         'score_at',
-         'score',
-         'score_percent')
+        conn, "score_history", ("node_id", "score_at", "score", "score_percent")
     )
-    score_history.fetch('score_at >= %s AND score_at IS NOT NULL', (args.last_update, ))
+    score_history.fetch("score_at >= %s AND score_at IS NOT NULL", (args.last_update,))
     score_history.print()
 
-    print('COMMIT;')
+    print("COMMIT;")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main(parse_args())
